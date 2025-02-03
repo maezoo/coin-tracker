40b96f37a5328f77b412cc932f2fd9651cf52d69
maezoo <aa00456@naver.com>
2025-02-03 18:19:58 +0900

    3.3 Optional Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -8,24 +9,32 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
-  bgColor: string;
+  bgColor: string; // required
+  borderColor?: string; // ? 붙이면: optional
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  // 만약 이 prop(optional)이 존재하지 않을 시 이 값(default text)을 넣어주세요
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
 
-interface PlayerShape {
+/* interface PlayerShape {
   name: string;
   age: number;
 }
 
-/* const sayHello = (playerObj: PlayerShape) =>
+const sayHello = (playerObj: PlayerShape) =>
   `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
 
 sayHello({ name: "nico", age: 12 });
