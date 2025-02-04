7f11ae12f605ce3b8371a6112c3fb85644cdd56c
maezoo <aa00456@naver.com>
2025-02-04 21:22:40 +0900

    4.5 useParams

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,30 +1,34 @@
-import { createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import About from "./screens/About";
 import Home from "./screens/Home";
 import Root from "./Root";
 import NotFound from "./screens/NotFound";
 import ErrorComponent from "./components/ErrorComponent";
+import User from "./screens/users/User";
 
 //createBrowserRouter() 자체가 하나의 함수이므로 반드시 괄호가 있어야 함
 //라우트 정보를 객체로 만들고([]) 그 안에 {} 객체를 넣은 것
 const router = createBrowserRouter([
   {
-    path:"/", 
+    path: "/",
     element: <Root />,
-    children:[
+    children: [
       {
-      path: "",
-      element: <Home />,
-      errorElement: <ErrorComponent />,
-    },
+        path: "",
+        element: <Home />,
+        errorElement: <ErrorComponent />,
+      },
       {
-        path:"about",
-      element: <About />,
-    },
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "users/:userId",
+        element: <User />,
+      },
     ],
     errorElement: <NotFound />, //에러 페이지 추가
   },
 ]);
-
 
 export default router;
