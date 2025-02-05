7f075fd3937ac5a9d9e2cec4a19d9edd8726c288
maezoo <aa00456@naver.com>
2025-02-05 13:39:59 +0900

    4.6 Outlet

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import Root from "./Root";
 import NotFound from "./screens/NotFound";
 import ErrorComponent from "./components/ErrorComponent";
 import User from "./screens/users/User";
+import Followers from "./screens/Followers";
 
 //createBrowserRouter() 자체가 하나의 함수이므로 반드시 괄호가 있어야 함
 //라우트 정보를 객체로 만들고([]) 그 안에 {} 객체를 넣은 것
@@ -25,6 +26,12 @@ const router = createBrowserRouter([
       {
         path: "users/:userId",
         element: <User />,
+        children: [
+          {
+            path: "followers",
+            element: <Followers />,
+          },
+        ],
       },
     ],
     errorElement: <NotFound />, //에러 페이지 추가
