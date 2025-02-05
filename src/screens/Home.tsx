f3c99a77bfdaf32da33d039b5930a0d3e330ad81
maezoo <aa00456@naver.com>
2025-02-05 19:51:25 +0900

    4.8 Extras

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,13 @@
 import { users } from "../db";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+
 function Home() {
+  const [readSearchParms, setSearchParams] = useSearchParams();
+
+  setTimeout(() => {
+    setSearchParams({ day: "today", tomorrow: "123" });
+  }, 3000);
+
   return (
     <div>
       <h1>Users</h1>
