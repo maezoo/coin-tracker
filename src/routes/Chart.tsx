1d3f2c0bd4ebaa023201b1bc33c2bf02ddca10a9
maezoo <aa00456@naver.com>
2025-02-10 21:08:56 +0900

    5.12 Price Chart Part Two

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,14 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { useParams } from "react-router";
+import { fetchCoinHistory } from "../api";
+
+interface ChartProps {
+  coinId: string;
+}
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery(["ohlv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
   return <h1>Chart</h1>;
 }
 
