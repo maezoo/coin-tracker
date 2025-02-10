a4a2899084a7b891a744b66acd0dc3d74bf15d29
maezoo <aa00456@naver.com>
2025-02-10 19:58:25 +0900

    5.10 React Query part Two

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -65,7 +65,7 @@ interface ICoin {
 
 function Coins() {
   const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
-
+  console.log(data);
   return (
     <Container>
       <Header>
